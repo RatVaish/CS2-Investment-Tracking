6d7afcce0193599f0eb279b5c73d2f6d88c929e5
RatVaish <vaishratul@gmail.com>
2026-01-14 01:42:27 +0000

    updated client.js to work on my server ip

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
+  baseURL: 'http://192.168.1.232:8000/api/v1',
   headers: {
     'Content-Type': 'application/json'
   }
